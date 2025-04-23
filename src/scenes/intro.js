24858e83a4d7a360b2cc6eccb49143af4defbd50
Damien BRIAND <damien.briand@st.com>
2025-04-23 15:49:09 +0200

    feat: transition screen and scene manager

---
+++ src/scenes/intro.js
@@ -0,0 +1,4 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { loadGLTF } from "../script/gltf_loader.js";
+import { HDRI_Loader } from "../script/hdri_loader.js";
