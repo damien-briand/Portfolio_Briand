019f787209aa336c50d5d463e0eb11052ff74331
Damien BRIAND <133599597+damien-briand@users.noreply.github.com>
2025-05-13 12:41:02 +0200

    Update intro.js

--- scenes/intro.js
+++ scenes/intro.js
@@ -66,7 +66,7 @@ async function intro_scene() {
 	let model;
 	let mixer;
 	try {
-		model = await loadGLTF("../assets/Intro_Scene.glb", scene);
+		model = await loadGLTF("/assets/Intro_Scene.glb", scene);
 		model.traverse((child) => {
 			if (child.isMesh) {
 				model.receiveShadow = true;
