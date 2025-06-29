b49083079a6d384a3d5365898021fc41520fd8c9
Damien BRIAND <damien.briand@etu.univ-grenoble-alpes.fr>
2025-06-29 12:07:41 +0200

    feat: image galerie

--- scenes/hub.js
+++ scenes/hub.js
@@ -65,7 +65,7 @@ async function hub_scene() {
 	let model;
 	let mixer;
 	try {
-		model = await loadGLTF("/Portfolio_Briand/assets/OS_HUB_scene.glb", scene);
+		model = await loadGLTF("/assets/OS_HUB_scene.glb", scene);
 		model.traverse((child) => {
 			if (child.isMesh) {
 				model.receiveShadow = true;
